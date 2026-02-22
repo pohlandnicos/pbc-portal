8824f891d92598ae778261262df2fe89b0e9fc0e
Nicos Pohland <n.p.pohland@gmail.com>
2026-02-22 13:33:32 +0100

    fix: add redirects for all app routes

--- next.config.js
+++ next.config.js
@@ -10,6 +10,36 @@ const nextConfig = {
         source: '/app/offers/:path*',
         destination: '/app/app/offers/:path*',
         permanent: true
+      },
+      {
+        source: '/app/customers/:path*',
+        destination: '/app/app/customers/:path*',
+        permanent: true
+      },
+      {
+        source: '/app/projects/:path*',
+        destination: '/app/app/projects/:path*',
+        permanent: true
+      },
+      {
+        source: '/app/invoices/:path*',
+        destination: '/app/app/invoices/:path*',
+        permanent: true
+      },
+      {
+        source: '/app/protocols/:path*',
+        destination: '/app/app/protocols/:path*',
+        permanent: true
+      },
+      {
+        source: '/app/tasks/:path*',
+        destination: '/app/app/tasks/:path*',
+        permanent: true
+      },
+      {
+        source: '/app/settings/:path*',
+        destination: '/app/app/settings/:path*',
+        permanent: true
       }
     ];
   }
