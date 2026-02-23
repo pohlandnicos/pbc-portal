429c96f74b3cdb7e1b268abcedf1bb8403f4d6f4
Nicos Pohland <n.p.pohland@gmail.com>
2026-02-23 15:25:43 +0100

    fix: simplify page structure and move metadata to layout

--- app/app/offers/new/layout.tsx
+++ app/app/offers/new/layout.tsx
@@ -1,7 +1,5 @@
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return children;
-}
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Neues Angebot erstellen',
+};
